2eb01be9e6f29249fe5fa6c026ce11d64f52e62c
januwA <ajanuw1641845087@sina.com>
2019-07-12 12:08:57 +0800

    优化

---
+++ built-value.js
@@ -0,0 +1,161 @@
+class BuiltValueAttr {
+  constructor(k, resultValue) {
+    this.k = k;
+    this.rs = resultValue;
+  }
+}
+class BuiltValue {
+  constructor(jsObject, rootName) {
+    this.jsObject = jsObject;
+    this.rootName = rootName;
+    this.resultObj = {};
+    this.makeResultArr(this.jsObject, this.rootName);
+    let resultString = this.makeResultString(this.resultObj);
+    resultString = this.addHeader(resultString);
+    return new String(resultString);
+  }
+
+  // 构建一个built_value树
+  makeResultArr(data, name) {
+    if (_.isObjectLike(data)) {
+      if (_.isArray(data)) {
+        data = _.first(data);
+      }
+      let resultObj = this.resultObj;
+      name = _.upperFirst(name);
+      resultObj[name] = {};
+      resultObj[name]["keys"] = [];
+      for (let k in data) {
+        let v = data[k];
+        let type = typeof v;
+        if (type === "object") {
+          if (_.isNull(v)) {
+            // null
+            let dartType = this.createDartType(v);
+            resultObj[name]["keys"].push(
+              new BuiltValueAttr(k, this.makeBuiltValueAttr(dartType, k))
+            );
+          } else if (_.isArray(v)) {
+            if (_.isEmpty(v)) return;
+            // value is array
+            // get type of first a value
+            let firstv = _.first(v);
+            if (_.isArray(firstv))
+              return alert(`data error: [ ${JSON.stringify(firstv)}, ...]`);
+            if (typeof firstv !== "object") {
+              // string, number, boolean
+              let dartType = this.createDartType(firstv);
+              resultObj[name]["keys"].push(
+                new BuiltValueAttr(
+                  k,
+                  this.makeBuiltValueAttr(dartType, k, true)
+                )
+              );
+            } else {
+              let dartType = _.upperFirst(k + "Dto");
+              resultObj[name]["keys"].push(
+                new BuiltValueAttr(
+                  k,
+                  this.makeBuiltValueAttr(dartType, k, true)
+                )
+              );
+              this.makeResultArr(firstv, k + "Dto");
+            }
+          } else if (_.isPlainObject(v)) {
+            // object
+            let dartType = _.upperFirst(k + "Dto");
+            resultObj[name]["keys"].push(
+              new BuiltValueAttr(k, this.makeBuiltValueAttr(dartType, k))
+            );
+            this.makeResultArr(v, k + "Dto");
+          }
+        } else {
+          // string, number, boolean, undefined
+          let dartType = this.createDartType(v);
+          resultObj[name]["keys"].push(
+            new BuiltValueAttr(k, this.makeBuiltValueAttr(dartType, k))
+          );
+        }
+      }
+    }
+  }
+
+  // 把built_value树，转化为string
+  makeResultString(obj) {
+    let resultString = ``;
+    for (const k in obj) {
+      let v = obj[k];
+
+      let attrs = ``;
+      for (const key of v.keys) {
+        attrs += key["rs"];
+      }
+
+      resultString += `
+/// ${k}
+abstract class ${k} implements Built<${k}, ${k}Builder> {
+  ${k}._();
+
+  factory ${k}([updates(${k}Builder b)]) = _$${k};
+  ${attrs}
+
+  String toJson() {
+    return jsonEncode(serializers.serializeWith(${k}.serializer, this));
+  }
+
+  static ${k} fromJson(String jsonString) {
+    return serializers.deserializeWith(
+        ${k}.serializer, jsonDecode(jsonString));
+  }
+
+  static Serializer<${k}> get serializer => _$${_.lowerFirst(k)}Serializer;
+}\r\n`;
+    }
+    return resultString;
+  }
+
+  // 添加头文件
+  addHeader(resultString) {
+    let name = _.snakeCase(this.rootName);
+    let header = `
+library ${name};
+
+import 'dart:convert';
+import 'package:built_collection/built_collection.dart';
+import 'package:built_value/built_value.dart';
+import 'package:built_value/serializer.dart';
+
+part '${name}.g.dart';
+  `;
+
+    return header + resultString;
+  }
+
+  makeBuiltValueAttr(dartType, k, isList = false) {
+    return !isList
+      ? `${dartType == "Null" ? "\r\n  @nullable" : ""}
+  @BuiltValueField(wireName: '${k}')
+  ${dartType} get ${_.camelCase(k)};
+`
+      : `
+  @BuiltValueField(wireName: '${k}')
+  BuiltList<${dartType}> get ${_.camelCase(k)};
+`;
+  }
+  // dart type
+  createDartType(v) {
+    let dartType = "";
+    if (_.isInteger(v)) {
+      dartType = "int";
+    } else if (_.isString(v)) {
+      dartType = "String";
+    } else if (_.isBoolean(v)) {
+      dartType = "bool";
+    } else if (_.isNull(v) || _.isUndefined(v)) {
+      dartType = "Null";
+    } else {
+      dartType = "double";
+    }
+    return dartType;
+  }
+}
--- sketch.js
+++ sketch.js
@@ -1,12 +1,9 @@
 const l = console.log;
 let classnameInput;
-let jsobjInput;
-
 let transformBtn;
-
+let jsobjInput;
 let built_valueOutput;
 let selectEle;
-let resultObj = {};
 let jsCodeMirror;
 let dartCodeMirror;
 
@@ -73,139 +70,10 @@ function setup() {
 
 // 点击转换按钮
 function transform() {
-  resultObj = {};
-  try {
-    let parse = getParse();
-    let rootName = classnameInput.value().trim();
-    makeResultArr(parse, rootName);
-    let resultString = makeResultString(resultObj);
-    resultString = addHeader(resultString);
-    // l(resultString);
-    dartCodeMirror.setValue(resultString.trim());
-  } catch (er) {
-    alert(er);
-  }
-}
-
-// 制作数据
-function makeResultArr(data, name) {
-  if (_.isPlainObject(data)) {
-    name = _.upperFirst(name);
-    resultObj[name] = {};
-    resultObj[name]["keys"] = [];
-    for (let k in data) {
-      let v = data[k];
-      let type = typeof v;
-      if (type === "object") {
-        if (_.isNull(v)) {
-          // null
-          let dartType = createDartType(v);
-          resultObj[name]["keys"].push(
-            new BuiltValueAttr(k, makeBuiltValueAttr(dartType, k))
-          );
-        } else if (_.isArray(v)) {
-          // array
-          if (_.isEmpty(v)) return alert("array is empty!");
-          // value is array
-          // get type of first a value
-          let firstv = _.first(v);
-          if (typeof firstv !== "object") {
-            // string, number, boolean
-            let dartType = createDartType(firstv);
-            resultObj[name]["keys"].push(
-              new BuiltValueAttr(k, makeBuiltValueAttr(dartType, k, true))
-            );
-          } else {
-            let dartType = _.upperFirst(k + "Dto");
-            resultObj[name]["keys"].push(
-              new BuiltValueAttr(k, makeBuiltValueAttr(dartType, k, true))
-            );
-            makeResultArr(firstv, k + "Dto");
-          }
-        } else if (_.isPlainObject(v)) {
-          // object
-          let dartType = _.upperFirst(k + "Dto");
-          resultObj[name]["keys"].push(
-            new BuiltValueAttr(k, makeBuiltValueAttr(dartType, k))
-          );
-          makeResultArr(v, k + "Dto");
-        }
-      } else {
-        // string, number, boolean, undefined
-        let dartType = createDartType(v);
-        resultObj[name]["keys"].push(
-          new BuiltValueAttr(k, makeBuiltValueAttr(dartType, k))
-        );
-      }
-    }
-  } else {
-    return alert("value is not plain object");
-  }
-}
-
-// 制作string
-function makeResultString(obj) {
-  let resultString = ``;
-  for (const k in obj) {
-    let v = obj[k];
-
-    let attrs = ``;
-    for (const key of v.keys) {
-      attrs += key["rs"];
-    }
-
-    resultString += `
-/// ${k}
-abstract class ${k} implements Built<${k}, ${k}Builder> {
-  ${k}._();
-
-  factory ${k}([updates(${k}Builder b)]) = _$${k};
-  ${attrs}
-
-  String toJson() {
-    return jsonEncode(serializers.serializeWith(${k}.serializer, this));
-  }
-
-  static ${k} fromJson(String jsonString) {
-    return serializers.deserializeWith(
-        ${k}.serializer, jsonDecode(jsonString));
-  }
-
-  static Serializer<${k}> get serializer => _$${_.lowerFirst(k)}Serializer;
-}\r\n`;
-  }
-  return resultString;
-}
-
-// make built_value attr
-function makeBuiltValueAttr(dartType, k, isList = false) {
-  return !isList
-    ? `${dartType == "Null" ? "\r\n  @nullable" : ""}
-  @BuiltValueField(wireName: '${k}')
-  ${dartType} get ${_.camelCase(k)};
-`
-    : `
-  @BuiltValueField(wireName: '${k}')
-  BuiltList<${dartType}> get ${_.camelCase(k)};
-`;
-}
-
-// file header
-function addHeader(resultString) {
-  let name = classnameInput.value().trim();
-  name = _.snakeCase(name);
-  let header = `
-library ${name};
-
-import 'dart:convert';
-import 'package:built_collection/built_collection.dart';
-import 'package:built_value/built_value.dart';
-import 'package:built_value/serializer.dart';
-
-part '${name}.g.dart';
-  `;
-
-  return header + resultString;
+  let jsObject = getParse();
+  let rootName = classnameInput.value().trim();
+  let builtValue = new BuiltValue(jsObject, rootName);
+  dartCodeMirror.setValue(builtValue.trim());
 }
 
 // object string or JSON
@@ -221,23 +89,6 @@ function getParse() {
   return parse;
 }
 
-// dart type
-function createDartType(v) {
-  let dartType = "";
-  if (_.isInteger(v)) {
-    dartType = "int";
-  } else if (_.isString(v)) {
-    dartType = "String";
-  } else if (_.isBoolean(v)) {
-    dartType = "bool";
-  } else if (_.isNull(v) || _.isUndefined(v)) {
-    dartType = "Null";
-  } else {
-    dartType = "double";
-  }
-  return dartType;
-}
-
 function selectChanged() {
   let v = selectEle.value().trim();
   if (v == 1) {
@@ -246,12 +97,5 @@ function selectChanged() {
   } else if (v == 2) {
     textareaObjText = jsCodeMirror.getValue().trim();
     jsCodeMirror.setValue(textareaJsonText);
-  }
-}
-
-class BuiltValueAttr {
-  constructor(k, resultValue) {
-    this.k = k;
-    this.rs = resultValue;
   }
 }
