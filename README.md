18ecf4128e6af76d4d7aa9bbf504be982f40537a
januwA <ajanuw1995@gmail.com>
2020-10-19 14:59:43 +0800

    save output to file.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-将js对象，或者json转化为，dart [built_value](https://pub.flutter-io.cn/packages/built_value) 对象
+将js对象，或者json转化为，dart [built_value](https://pub.flutter-io.cn/packages/built_value) dto
 
-![](./docs/demo.png)
+![](./images/2020-10-19-14-57-47.png)
--- js/sketch.js
+++ js/sketch.js
@@ -15,6 +15,8 @@ let serializersOutput;
 // select
 let selectEle;
 
+let saveBtn;
+
 // editor
 let jsCodeMirror;
 let dartCodeMirror;
@@ -31,30 +33,33 @@ function setup() {
   built_valueOutput = select("#built_value");
   serializersOutput = select("#serializers");
   selectEle = select("#select");
+  saveBtn = select("#save");
 
   jsCodeMirror = CodeMirror.fromTextArea(jsobjInput.elt, {
     lineNumbers: true,
     mode: "javascript",
-    theme: "dracula"
+    theme: "dracula",
   });
 
   dartCodeMirror = CodeMirror.fromTextArea(built_valueOutput.elt, {
     lineNumbers: true,
     mode: "javascript",
-    theme: "dracula"
+    theme: "dracula",
   });
 
   serializersCodeMirror = CodeMirror.fromTextArea(serializersOutput.elt, {
     lineNumbers: true,
     mode: "javascript",
-    theme: "dracula"
+    theme: "dracula",
   });
 
   transformBtn.mouseClicked(transform);
   selectEle.changed(selectChanged);
+  saveBtn.mouseClicked(saveDtoStr2File);
 }
 
 // 点击转换按钮
+// output textarea
 function transform() {
   const value = jsCodeMirror.getValue().trim();
   const jsObject = JSON5.parse(value);
@@ -64,6 +69,7 @@ function transform() {
   serializersCodeMirror.setValue(new BuildSerializers(ROOtNAME).trim());
 }
 
+// input textarea
 function selectChanged() {
   switch (+selectEle.value().trim()) {
     case 1:
@@ -78,4 +84,46 @@ function selectChanged() {
     default:
       break;
   }
+}
+
+// save output to file.
+async function saveDtoStr2File() {
+  if (!window.showDirectoryPicker) {
+    return alert("not showDirectoryPicker API");
+  }
+
+  let dtoText = dartCodeMirror.getValue().trim();
+  let serializersText = serializersCodeMirror.getValue().trim();
+
+  if (!dtoText || !serializersText) {
+    return alert("not data. to click transform button.");
+  }
+
+  let dtoFileName = _.snakeCase(ROOtNAME) + ".dart";
+  let serializersFileName = "serializers.dart";
+  let saveDirName = ROOtNAME.trim();
+
+  // 获取目录句柄
+  const hDir = await window.showDirectoryPicker();
+
+  // 创建新目录，如果存在直接覆盖
+  const hNewDir = await hDir.getDirectoryHandle(saveDirName, {
+    create: true,
+  });
+
+  // dto文件
+  let hNewFile = await hNewDir.getFileHandle(dtoFileName, {
+    create: true,
+  });
+  let w$ = await hNewFile.createWritable();
+  await w$.write(dtoText);
+  await w$.close();
+
+  // serializers文件
+  hNewFile = await hNewDir.getFileHandle(serializersFileName, {
+    create: true,
+  });
+  w$ = await hNewFile.createWritable();
+  await w$.write(serializersText);
+  await w$.close();
 }
